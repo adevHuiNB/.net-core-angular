96f7a99768983a2d1ad98e5310de1d0b5ecfa5e1
luojihencha <oklimengyue@gmail.com>
2021-12-09 00:37:16 -0500

    return data from StoreContext to API requests

--- skinet/API/Controllers/ProductsController.cs
+++ skinet/API/Controllers/ProductsController.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -11,14 +13,21 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            this._context = context;
+        }
+
         [HttpGet]
-        public string GetProducts(){
-            return "this is a list of product";
+        public async Task<ActionResult<List<Product>>> GetProducts(){
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
         }
 
         [HttpGet("{id}")]
-        public string GetProduct(int id){
-            return "this is a product";
+        public async Task<ActionResult<Product>> GetProduct(int id){
+            return await _context.Products.FindAsync(id);
 
         }
     }
